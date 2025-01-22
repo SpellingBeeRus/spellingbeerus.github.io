76d68fde3975d529aa4209d82426019a69606dc5
rimuruyeban <eventskritniyagent@gmail.com>
2025-01-22 23:21:39 +0500

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,12 @@ export default defineConfig({
     host: true,
     fs: {
       strict: false
+    },
+    proxy: {
+      '/socket.io': {
+        target: 'http://localhost:3001',
+        ws: true
+      }
     }
   },
   build: {

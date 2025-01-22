07bf8b354f0b57e17fd72d2aa71db2eca4cc475b
rimuruyeban <eventskritniyagent@gmail.com>
2025-01-22 23:46:02 +0500

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/beespelling/',
+  base: '/',
   server: {
     port: 3000,
     host: true,
